017f23c43ce4eb9450499d11623516f0bfffd0db
Husam Jbr <husamjbr.prg@outlook.com>
2022-04-16 21:08:17 +0300

    fifth version

--- src/main/java/com/atypon/games/chess_game/utility/ChessMovement.java
+++ src/main/java/com/atypon/games/chess_game/utility/ChessMovement.java
@@ -5,40 +5,49 @@ import com.atypon.pieces.*;
 
 public class ChessMovement {
 
-    private static void ifCastlingThenPerform(ChessBoard chessBoard, Spot from, Spot to){
-        if(from.getPiece() instanceof King){
-           if(Math.abs(from.getCoordinates().getY()-to.getCoordinates().getY()) == 2){
-              if(to.getCoordinates().getY() > from.getCoordinates().getY()) { // kingSide castling
-                    performMove(chessBoard, chessBoard.getSpot(from.getCoordinates().getX(),7), chessBoard.getSpot(from.getCoordinates().getX(),5));
-              }else{ // queenSide castling
-                    performMove(chessBoard, chessBoard.getSpot(from.getCoordinates().getX(),0), chessBoard.getSpot(from.getCoordinates().getX(), 3));
-              }
-           }
-        }
+    private static boolean isCastling(Spot from, Spot to){
+        return (from.getPiece() instanceof King
+                && Math.abs(from.getCoordinates().getY()-to.getCoordinates().getY()) == 2);
     }
-    private static void ifPromotionThenPerform(Spot from, Spot to){
-        if(from.getPiece() instanceof Pawn){
-           if(from.getPiece().getColor() == Color.WHITE
-                   && from.getCoordinates().getX() == 6 && to.getCoordinates().getX() == 7){ // it's a promotion
-                from.setPiece(new Queen(from.getPiece().getColor()));
-           }else if(from.getPiece().getColor() == Color.BLACK
-                   && from.getCoordinates().getX() == 1 && to.getCoordinates().getX() == 0){
-               from.setPiece(new Queen(from.getPiece().getColor()));
-           }
+    private static void performCastling(ChessBoard chessBoard, Spot from, Spot to){
+        Spot rookSpot;
+        Spot newRookSpot;
+        if(to.getCoordinates().getY() > from.getCoordinates().getY()) { // kingSide castling
+            rookSpot = chessBoard.getSpot(from.getCoordinates().getX(), 7);
+            newRookSpot = chessBoard.getSpot(from.getCoordinates().getX(), 5);
+        }else{ // queenSide castling
+            rookSpot = chessBoard.getSpot(from.getCoordinates().getX(), 0);
+            newRookSpot = chessBoard.getSpot(from.getCoordinates().getX(), 3);
         }
+        performMove(chessBoard, rookSpot, newRookSpot);
+    }
+    private static boolean isPromotion(Spot from, Spot to){
+        return (from.getPiece() instanceof Pawn
+                && from.getPiece().getColor() == Color.WHITE
+                && to.getCoordinates().getX() == 7
+                || from.getPiece().getColor() == Color.BLACK
+                && to.getCoordinates().getX() == 0);
+    }
+    private static void performPromotion(Spot from){
+        from.setPiece(new Queen(from.getPiece().getColor()));
     }
     private static void performMove(ChessBoard chessBoard, Spot from, Spot to){
         to.setPiece(from.getPiece());
         to.getPiece().move();
-        /*
-        remove and add new underAttack spots
-         */
         from.setPiece(new NoPiece());
     }
     public static void move(ChessBoard chessBoard, Spot from, Spot to){
-        ifCastlingThenPerform(chessBoard, from, to);
-        ifPromotionThenPerform(from, to);
+        /*
+        remove underAttack spots
+         */
+        if(isCastling(from,to))
+            performCastling(chessBoard, from, to);
+        if(isPromotion(from, to))
+            performPromotion(from);
         performMove(chessBoard, from, to);
+        /*
+        add underAttack spots
+         */
         System.out.println();
         System.out.println(chessBoard);
     }
